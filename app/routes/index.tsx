9b4515bb70c6e01ef31c8af43bb6194fd72ac2cb
Juan D. Jara <juanorigami@gmail.com>
2022-10-20 13:20:02 +0200

    add public login disclaimer

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -14,7 +14,7 @@ export async function loader({ request }: LoaderArgs) {
 
 export default function Index() {
   return (
-    <div className="my-6 max-w-md mx-auto flex flex-col items-center justify-center">
+    <div className="p-3 my-6 max-w-md mx-auto flex flex-col items-center justify-center">
       <p className="text-6xl">🐷</p>
       <h1 className="text-slate-500 dark:text-slate-300 font-medium text-2xl font-serif">
         <em>Press</em>unto
@@ -22,10 +22,17 @@ export default function Index() {
       <p className="text-lg my-6 text-center">
         Pressunto is a content editor for GitHub designed for managing markdown files in static websites.
       </p>
-      <p className="mt-6 mb-2 text-slate-500 dark:text-slate-300">
-        Log in with GitHub
-      </p>
-      <LoginButton />
+      <div>
+        <p className="mt-6 mb-2 text-slate-500 dark:text-slate-300">
+          Log in with GitHub
+        </p>
+        <LoginButton />
+        <p className="text-sm my-4">
+          Your login data will be used only to read the user name and profile picture, which organizations the user belongs to and to read and write code from the repositories (public or private). No other information will be accesed, such as issues, PRs, discussions, actions or other keys and settings.
+          For more information on how data is accesed, you can check the{' '}
+          <a className="underline" href="https://github.com/juandjara/pressunto/blob/master/app/lib/github.ts">public source code</a>
+        </p>
+      </div>
     </div>
   )
 }
