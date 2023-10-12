b14a151847072fedccd2e2f19f3eb0e20027396c
Juan D. Jara <juanorigami@gmail.com>
2023-10-12 18:47:47 +0200

    add little margin to the body

--- app/root.tsx
+++ app/root.tsx
@@ -87,7 +87,7 @@ export default function App() {
         <div className="lg:container mx-auto min-h-screen relative">
           <FlashMessage />
           <Header user={user} />
-          <div className="bg-slate-50 dark:bg-slate-800 rounded-md shadow">
+          <div className="bg-slate-50 dark:bg-slate-800 rounded-md shadow mb-2">
             <Outlet />
           </div>
         </div>
